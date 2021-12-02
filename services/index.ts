a7760eb3f4bdfa5bb6f6582a023c0c20cb045a09
Youssef <youssef@Youssefs-MacBook-Pro.local>
2021-12-02 23:04:39 +0330

    fix(service): change timeout

--- services/index.ts
+++ services/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const restCountriesClient = axios.create({
   baseURL: 'https://restcountries.com/v3.1',
-  timeout: 1000
+  timeout: 10000
 });
 
 export { countriesAPI } from './countries';
